663168b0e91723ce227de8d0ef51bea5fe96dbbf
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-04-25 17:21:33 +0200

    add stretch task 1

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const hueInput = process.argv[2];
 const luminosityInput = process.argv[3];
 let specialColor = '';
 
-if (hueInput && luminosityInput) {
+if (hueInput === 'ask') {
+  console.log(
+    `\x1b[38;2;0;255;127mYou can do it like this instead: $ node index blue dark\x1b[0m`,
+  ); // rgb(0,255,127)
+} else if (hueInput && luminosityInput) {
   specialColor = randomColor({
     luminosity: luminosityInput,
     hue: hueInput,
